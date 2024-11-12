7c2eee38220967805347ccc378f8aa73db6784ca
kacikopf <kopf0017@umn.edu>
2024-11-11 23:01:59 -0600

    Created a generic card component containing a slot. Created a membership card component with props inside the slot of the generic card component.

---
+++ src/components/GenericCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  blockColor: {
+    type: String,
+    required: false,
+    default: '#f17a8e'
+  }
+})
+</script>
+
+<template>
+  <section class="generic-card" :style="'background-color: ' + blockColor + ';'">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+.generic-card {
+  box-shadow: 15px 15px #272727;
+  padding: 16px;
+}
+</style>
---
+++ src/components/Membership-Card.vue
@@ -0,0 +1,78 @@
+<script setup>
+
+
+defineProps({
+  membershipName: {
+    type: String,
+    required: true
+  },
+
+  membershipCost: {
+    type: String,
+    required: true
+  },
+
+  membershipType: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+
+  <section class="membership-card">
+    <h1 class="membership-name">{{ membershipName }}</h1>
+    <p class="membership-cost" v-html="membershipCost"></p>
+    <p class="membership-type" v-html="membershipType"></p>
+  </section>
+
+
+</template>
+
+<style scoped>
+section.membership-card {
+  display: flex;
+  flex-direction: column;
+  color: white;
+
+  h1, a, p {
+    font-family: "Azo Sans", Helvetica, sans-serif;
+    margin: .5rem;
+  }
+
+  h1 {
+    font-family: "Azo Sans", Helvetica, sans-serif;
+    font-weight: bold;
+    font-size: 2em;
+    text-transform: uppercase;
+  }
+}
+</style>
+
+<style>
+.membership-cost {
+  span {
+    font-weight: bold;
+    font-size: 2em;
+  }
+
+  span2 {
+    font-weight: bold;
+    font-size: 2em;
+    color: #272727;
+  }
+
+  span3 {
+    color: #272727;
+  }
+}
+
+.membership-type {
+  span {
+    font-weight: bold;
+    color: #272727;
+  }
+}
+</style>

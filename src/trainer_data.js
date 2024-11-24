c8f0a0ec4f62300389e046a398a51fb0c2ee45a2
kacikopf <kopf0017@umn.edu>
2024-11-24 15:07:20 -0600

    WIP: Created a mock data file for the personal trainer info

---
+++ src/trainer_data.js
@@ -0,0 +1,29 @@
+const data =
+[
+  {
+    "id": 1,
+    "photo": "http://dummyimage.com/210x300.png/dddddd/000000",
+    "first_name": "Tabatha",
+    "last_name": "Peachment",
+    "description": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+    "certifications": "item 3"
+  },
+  {
+    "id": 2,
+    "photo": "http://dummyimage.com/210x300.png/dddddd/000000",
+    "first_name": "Ottilie",
+    "last_name": "Wignall",
+    "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    "certifications": "item 2"
+  },
+  {
+    "id": 3,
+    "photo": "http://dummyimage.com/210x300.png/ff4444/ffffff",
+    "first_name": "Glenda",
+    "last_name": "Butterfill",
+    "description": "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    "certifications": "item 1"
+  }
+]
+
+export default {data}
